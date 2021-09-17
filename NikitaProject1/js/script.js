c33f526340a16d430f63d1ae668764b119f1b7c0
Nikita <s2015836@x.donstu.ru>
2021-09-17 19:39:13 +0300

    Nikita first homework

--- NikitaProject1/js/script.js
+++ NikitaProject1/js/script.js
@@ -1,4 +1,17 @@
 "use strict";
-const user = "Ivan";
 
-alert(`Привет, ${user}`);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let film1 = prompt('Один из последних просмотренных фильмов?');
+personalMovieDB.movies[film1] = prompt('На сколько оцените его?');
+let film2 = prompt('Один из последних просмотренных фильмов?');
+personalMovieDB.movies[film2] = prompt('На сколько оцените его?');
+console.log(personalMovieDB);
