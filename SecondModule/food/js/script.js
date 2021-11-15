3edacff69dc6863f4e4db6381911974d9701bedf
Nikita <s2015836@x.donstu.ru>
2021-11-15 22:47:39 +0300

    41. fixed two bugs in the project food

--- SecondModule/food/js/script.js
+++ SecondModule/food/js/script.js
@@ -37,29 +37,38 @@ document.addEventListener('DOMContentLoaded', () => {
               hours = Math.floor((t / (1000 * 60 * 60)) % 24),
               minutes = Math.floor((t / (1000 * 60)) % 60),
               seconds = Math.floor((t / 1000) % 60);
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
+        return { //быстрая запись свойств объекта в ES6 
+            t,
+            days, //эквивалентно 'days': days
+            hours,
+            minutes,
+            seconds
         };
     }
 
-    function setClock (clockSelector, endTime) {
+    function getZero(a) { //если число меньше 10, добавлем 0 перед ним
+        if (a >= 0 && a < 10) {
+            a = `0${a}`;
+        }
+        return a;
+    }
+
+    function setClock(clockSelector, endTime) {
         const timer = document.querySelector(clockSelector), //Записываем элементы таймера в переменные
               days = timer.querySelector('#days'),
               hours = timer.querySelector('#hours'),
               minutes = timer.querySelector('#minutes'),
               seconds = timer.querySelector('#seconds'),
               timeInterval = setInterval(updateClock, 1000); //запускаем функцию обновления таймера с интервалом в сек
-            
+        
+        updateClock(); //запускаем один раз обновление часов, чтобы не было мигания в начале    
+        
         function updateClock () { //записываем время из объекта в элементы таймера
             const t = getTimeRemaining(endTime);
-            days.innerHTML = t.days;
-            hours.innerHTML = t.hours;
-            minutes.innerHTML = t.minutes;
-            seconds.innerHTML = t.seconds;
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
             if (t.t <= 0) {
                 clearInterval(timeInterval);
             }
