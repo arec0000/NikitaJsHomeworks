670c570ffa70e9f63c6b4c9a69d7333cdd9d3550
Nikita <s2015836@x.donstu.ru>
2021-12-08 21:45:05 +0300

    delete unnecessary jshintrc files

--- food/js/script.js
+++ food/js/script.js
@@ -181,43 +181,93 @@ class MenuItem {
 
 //Forms
 
-const forms = document.querySelectorAll('form');
+// const forms = document.querySelectorAll('form');
 
-const messege = {
-    loading: 'Загрузка',
-    success: 'Спасибо',
-    failure: 'Чё-то пошло не так'
-};
+// const message = {
+//     loading: 'Загрузка',
+//     success: 'Спасибо',
+//     failure: 'Чё-то пошло не так'
+// };
 
-forms.forEach(item => {
-    postData(item);
-});
+// forms.forEach(item => {
+//     postData(item);
+// });
 
-function postData(form) {
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
+// function postData(form) {
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault();
 
-        const statusMessage = document.createElement('div');
-        statusMessage.classList.add('status');
-        statusMessage.textContent = messege.loading;
-        form.append(statusMessage);
+//         let statusMessage = document.createElement('div');
+//         statusMessage.classList.add('status');
+//         statusMessage.textContent = message.loading;
+//         form.append(statusMessage);
 
-        const request = new XMLHttpRequest();
+//         const request = new XMLHttpRequest();
 
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-type', 'multipart/form-data');
+//         request.open('POST', 'server.php');
+//         request.setRequestHeader('Content-type', 'application/json');
 
-        const formData = new FormData(form);
+//         const formData = new FormData(form);
 
-        request.send(formData);
+//         request.send(formData);
 
-        request.addEventListener('load', () => {
-            if (request.status === 200) {
-                console.log(request.response);
-                statusMessage.textContent = messege.success;
-            } else {
-                statusMessage.textContent = messege.failure;
-            }
-        });
+//         request.addEventListener('load', () => {
+//             if (request.status === 200) {
+//                 console.log(request.response);
+//                 statusMessage.textContent = message.success;
+//             } else {
+//                 statusMessage.textContent = message.failure;
+//             }
+//         });
+//     });
+// }
+
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
     });
-}
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.appendChild(statusMessage);
+        
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
